39e6738ab75459d185ea0f185e600bccc0f1f551
Luka Horvatič <luka.horvatic7@gmail.com>
2017-10-02 11:43:40 +0200

    Spring app - code cleanup

--- src/main/java/nfg/controller/IzdelekController.java
+++ src/main/java/nfg/controller/IzdelekController.java
@@ -10,9 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-//import io.swagger.annotations.ApiOperation;
 import nfg.model.Izdelek;
-//import nfg.repository.InMemoryProductRepository;
 import nfg.repository.IzdelekRepository;
 
 @RestController
@@ -24,25 +22,21 @@ public class IzdelekController {
 	
 	@RequestMapping(value = "izdelki", method = RequestMethod.GET)
 	public List<Izdelek> list() {
-//		return InMemoryProductRepository.list();
 		return izdelekRepository.findAll();
 	}
 	
 	@RequestMapping(value = "izdelki", method = RequestMethod.POST)
 	public Izdelek create(@RequestBody Izdelek izdelek) {
-//		return InMemoryProductRepository.create(izdelek);
 		return izdelekRepository.saveAndFlush(izdelek);
 	}
 
 	@RequestMapping(value = "izdelki/{id}", method = RequestMethod.GET)
 	public Izdelek get(@PathVariable Long id) {
-//		return InMemoryProductRepository.get(id);
 		return izdelekRepository.findOne(id);
 	}
 	
 	@RequestMapping(value = "izdelki/{id}", method = RequestMethod.PUT)
 	public Izdelek update(@PathVariable Long id, @RequestBody Izdelek izdelek) {
-//		return InMemoryProductRepository.update(id, izdelek);
 		Izdelek i = izdelekRepository.findOne(id);
 		BeanUtils.copyProperties(izdelek, i);
 		return izdelekRepository.saveAndFlush(i);
@@ -50,7 +44,6 @@ public class IzdelekController {
 	
 	@RequestMapping(value = "izdelki/{id}", method = RequestMethod.DELETE)
 	public Izdelek delete(@PathVariable Long id) {
-//		return InMemoryProductRepository.delete(id);
 		Izdelek i = izdelekRepository.findOne(id);
 		izdelekRepository.delete(i);
 		return i;
